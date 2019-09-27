ceba8109ed1f49ee0bafb14d4f7a524f72d8efc8
shararezr <zr.sharare@gmail.com>
2019-09-27 09:50:12 +0330

    Add files via upload

---
+++ NB/Feature_Extractor.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+def feature_extractor(x_train , x_test):
+
+	pca = PCA (n_components = 0.99 ,  svd_solver = "full")
+	pca.fit(x_train)
+
+	return pca.transform(x_train) , pca.transform(x_test)
