ee224c7b8f315eb80c6a99042a8064f384b92490
shararezr <zr.sharare@gmail.com>
2019-09-27 09:48:28 +0330

    Add files via upload

---
+++ hw3/svm/Classifier.py
@@ -0,0 +1,13 @@
+from sklearn import svm
+
+
+def classifier(x_train  , y_train , x_test , y_test):
+	y_train = y_train.astype(int)
+	y_test = y_test.astype(int)
+	x_train = x_train[:6000]
+	y_train = y_train[:6000]
+	x_test = x_test[:1600]
+	y_test = y_test[:1600]
+	clf = svm.SVC(gamma = "scale")
+	clf.fit(x_train , y_train)
+	return clf.score(x_test , y_test)
